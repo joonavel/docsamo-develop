4552ec2008ca567b14fbaf588979339018c60fa7
joonavel <joonavel@gmail.com>
2024-07-28 17:08:32 +0900

    ✨ Stateful Streamlit
    session_state를 이용해서 페이지가 바뀌더라도 특정 정보들이 초기화 되지 않도록 구현했습니다.

--- pages/ending.py
+++ pages/ending.py
@@ -5,4 +5,6 @@ st.set_page_config("ending page.")
 print("ending session state:", st.session_state)
 st.write("ending page")
 
+st.session_state.game_page = "ending"
+
 show_sidebar()
--- pages/settings.py
+++ pages/settings.py
@@ -4,4 +4,8 @@ from utils import switch_page, show_sidebar
 print("settings session state:", st.session_state)
 st.write("page2")
 
+if st.button("게임으로 돌아가기"):
+    switch_page(st.session_state.game_page)
+
+
 show_sidebar()
