d488606efd40f61fd3fd25fcc4cac8b54e13a7f8
won7527 <wonheo7527@gmail.com>
2024-07-29 15:24:35 +0900

    ✨ Feat: Generate_False_Story

--- generate_question.py
+++ generate_question.py
@@ -1,14 +1,9 @@
 import os
 from dotenv import load_dotenv
-from langchain.chains import LLMChain
-from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.prompts import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-import re
+from langchain.chains import LLMChain
+import streamlit as st
+from utils import generate_prompt
 
 load_dotenv()
 
@@ -16,35 +11,32 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-4o-mini")
 
-
-def load_prompt(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        prompt = file.read().strip()
-    return prompt
-
-
 prompt_path = "prompts/generate_question.prompt"
-system_prompt = load_prompt(prompt_path)
-custom_prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(system_prompt),
-        HumanMessagePromptTemplate.from_template(
-            "{first_story}, {second_story}, {select}"
-        ),
-    ]
-)
+prompt = generate_prompt(prompt_path, "{first_story}, {second_story}, {select}")
+
 first_story = "옛날 어느 곳에 한 평민이 살았는데 산의 정기를 받아서 겨드랑이에 날개(비늘)가 있고 태어나자 이내 날아다니고 힘도 센 장수 아들을 기적적으로 낳았다.그런데 부모는 이 아기장수가 크면 장차 역적이 되어서 집안을 망칠 것이라고 해서 아기장수를 돌로 눌러 죽인다.아기장수가 죽을 때 유언으로 콩 다섯 섬과 팥 다섯 섬을 같이 묻어 달라고 하였다"
 second_story = "얼마후 관군이 와서 아기장수를 내놓으라고 하여 이미 부모가 죽었다고 하니 무덤을 가르쳐 달라고 하는 것을 그 어머니가 실토하여 가 보았더니 콩은 말이 되고 팥은 군사가 되어 아기장수가 막 일어나려고 하고 있었다. 그러나 그만 관군에게 들켜서 성공 직전에 죽임을 당하였다."
-select = "false"
+st.session_state.select = "false"
 
-chain = LLMChain(llm=llm, prompt=custom_prompt, verbose=True)
+chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 result = chain(
-    {"first_story": first_story, "second_story": second_story, "select": select}
+    {
+        "first_story": first_story,
+        "second_story": second_story,
+        "select": st.session_state.select,
+    }
 )
 
-pattern = r"\((.*?)\)"
-options = re.findall(pattern, result["text"])
-first_option, second_option = options[-2], options[-1]
-print(result["text"])
-print(first_option)
-print(second_option)
+# question과 option들을 session_state에 저장
+question_idx = result["text"].find("1")
+st.session_state.question = result["text"][:question_idx]
+first_option_idx = result["text"].find("2", question_idx)
+st.session_state.first_option = result["text"][question_idx:first_option_idx]
+st.session_state.second_option = result["text"][first_option_idx:]
+
+st.write(st.session_state.question)
+st.write(st.session_state.first_option)
+st.write(st.session_state.second_option)
+
+# 나중에 버튼이 생기면 없어질 부분
+import generate_false_story
